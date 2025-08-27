80ecabef7c9214464ba0e28b1fc56a6a0cb5f754
shimodi <am.shahmoradi93@gmail.com>
2025-08-27 23:07:40 +0330

    redis address changed to dockeri shodash :))

--- repository/redis.go
+++ repository/redis.go
@@ -1,14 +1,17 @@
 package repository
 
 import (
+	"os"
+
 	"github.com/redis/go-redis/v9"
 )
 
 var RedisDB *redis.Client
 
 func OpenRedis() {
+
 	redisdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     os.Getenv("REDIS_ADDR"),
 		Password: "",
 		DB:       0,
 	})
