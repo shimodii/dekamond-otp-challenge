d4465c0e635552a1d085455dcf7797ae5061e8b8
shimodi <am.shahmoradi93@gmail.com>
2025-08-27 23:06:08 +0330

    dsn changed based on docker stuff

--- repository/repository.go
+++ repository/repository.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+  "os"
+  "fmt"
+
 	"github.com/shimodii/dekamond-otp-challenge/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,8 +13,17 @@ var Database *gorm.DB
 
 func DatabaseConnection() {
 
+  // docker :D
+  dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
   // used from gorm docs :)
-  dsn := "host=localhost user=postgres password=password dbname=dekamond port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+  //dsn := "host=localhost user=postgres password=password dbname=dekamond port=5432 sslmode=disable TimeZone=Asia/Shanghai"
   db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
   
   if err != nil {
