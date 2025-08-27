9a004b29f4ab37d190c530f5b50596c430300471
shimodi <am.shahmoradi93@gmail.com>
2025-08-27 16:58:24 +0330

    created at field tested (yay)

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/shimodii/dekamond-otp-challenge/model"
 	"github.com/shimodii/dekamond-otp-challenge/repository"
@@ -19,6 +21,7 @@ func main() {
   application.Get("/test", func(c *fiber.Ctx) error {
     user := model.User{
       Phone: "9933345821",
+      CreatedAt: time.Now(),
     }
     repository.Database.Create(&user)
     return c.SendString("ok")
