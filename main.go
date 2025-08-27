9abe0070b334ba747c9e78886d83a450db60004b
shimodi <am.shahmoradi93@gmail.com>
2025-08-27 18:05:01 +0330

    some tests on redis

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,19 +12,27 @@ import (
 func main() {
   application := fiber.New()
 
+  ctx := context.Background()
+
   // database
   repository.DatabaseConnection()
+  repository.OpenRedis()
 
   application.Get("/", func(c *fiber.Ctx) error {
     return c.SendString("Hello world")
   })
 
+  // all tests are here
+  /*
+    i had no idea about redis so i opened this endpoint to test every single of my questions :D 
+  */
   application.Get("/test", func(c *fiber.Ctx) error {
     user := model.User{
       Phone: "9933345821",
       CreatedAt: time.Now(),
     }
     repository.Database.Create(&user)
+    repository.RedisDB.Set(ctx, user.Phone, "12345", 10*time.Second)
     return c.SendString("ok")
   })
 
